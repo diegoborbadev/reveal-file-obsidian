1349d109832969df94d5b0136604823ff2c0fe6e
Diego Borba <diegoborba25.contato@gmail.com>
2024-06-29 15:34:59 -0300

    feat: toggle button added

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
-import { Plugin } from 'obsidian';
+import { Plugin, Notice } from 'obsidian';
 
 export default class RevealFilePlugin extends Plugin {
+	private isToggled: boolean = false;
+
 	async onload(): Promise<void> {
-		this.registerEvent(this.app.workspace.on('file-open', () => {
-			(this.app as any).commands.executeCommandById('file-explorer:reveal-active-file');
-		}));
+		this.addRibbonIcon('view', 'Toggle reveal file plugin', this.toggle.bind(this));
+		this.registerEvent(this.app.workspace.on('file-open', this.revealActiveFileIfToggled.bind(this)));
 	}
 
 	async onunload(): Promise<void> {
 		console.log('Unloading RevealFilePlugin');
+	}
+
+	private toggle(): void {
+		new Notice(`File reveal toggle to ${this.isToggled ? 'OFF' : 'ON'}`);
+		this.isToggled = !this.isToggled;
+	}
+
+	private revealActiveFileIfToggled(): void {
+		if (this.isToggled) {
+			(this.app as any).commands.executeCommandById('file-explorer:reveal-active-file');
+		}
 	}
 }
