08ae26f5c5811295246d1a11ed9b95acc5792a85
Diego Borba <diegoborba25.contato@gmail.com>
2024-06-29 15:09:08 -0300

    fix: verbose return types

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 import { Plugin } from 'obsidian';
 
 export default class RevealFilePlugin extends Plugin {
-	async onload() {
+	async onload(): Promise<void> {
 		this.registerEvent(this.app.workspace.on('file-open', () => {
 			(this.app as any).commands.executeCommandById('file-explorer:reveal-active-file');
 		}));
 	}
 
-	onunload() {
+	async onunload(): Promise<void> {
 		console.log('Unloading RevealFilePlugin');
 	}
 }
